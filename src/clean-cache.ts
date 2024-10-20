53f2c2669a2cbd26051f82ec060b48159a2c29fb
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-10-20 20:26:11 +0900

    Revert "formatが間違ってたので直してあげたよ！ (#1132)"
    This reverts commit 2c6daf0567f90e9fd05efe8d407bafda6947fceb.

--- src/clean-cache.ts
+++ src/clean-cache.ts
@@ -1,13 +1,13 @@
 import { RequestError } from "@octokit/request-error";
 
 export async function script() {
-  try {
-    throw new Error("aaa");
-  } catch (e) {
-    if (e instanceof RequestError && e.status === 404) {
-      console.log(e);
-    }
+    try {
+      throw new Error("aaa");
+    } catch (e) {
+      if (e instanceof RequestError && e.status === 404) {
+        console.log(e);
+      }
 
-    throw e;
-  }
+      throw e;
+    }
 }
