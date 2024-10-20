b60b1fd88f2912117dfcf4adf18c1059d7302f9a
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-10-20 15:00:27 +0900

    CIで使用しているスクリプトをTypeScript化する

--- scripts/update_readme/update_readme/get_inputs_markdown.ts
+++ scripts/update_readme/update_readme/get_inputs_markdown.ts
@@ -1,8 +1,16 @@
-const fs = require("fs");
-const yaml = require("js-yaml");
+import {readFileSync} from "fs";
+import * as yaml from "js-yaml";
 
-module.exports = () => {
-  const ymlFile = yaml.load(fs.readFileSync("action.yml", "utf8"));
+export function script():string{
+  const ymlFile = yaml.load(readFileSync("action.yml", "utf8")) as {
+    inputs: {
+      [p: string]: {
+        default: string
+        description: string
+        required: "true" | "false"
+      }
+    }
+  };
   const inputs = ymlFile.inputs;
   const rows = ["| 引数名 | 説明 | 必須 |", "|:---:|:---:|:---:|"];
 
@@ -18,4 +26,4 @@ module.exports = () => {
   }
 
   return rows.join("\n");
-};
+}
