d7ad377230b695ac4b6e38212a1fa27bb21cd26b
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-10-20 20:20:19 +0900

    fix

--- scripts/update_dist/update_dist/build.sh
+++ scripts/update_dist/update_dist/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 npm ci
-npx esbuild --bundle --sourcemap --format=cjs --outdir=dist src/clean-cache.ts
+npx esbuild --bundle --sourcemap=inline --format=cjs --outdir=dist src/clean-cache.ts
